66fbe3edfb3c0cbe93c2af466aa74bd6f664f1d4
oliverd <dendis.oliver@gmail.com>
2017-07-14 10:38:40 +0200

    Admin logout
    Fungujuci admin logout, momentálne funguje podobne ako user, v dropdowne je možnosť logoutu

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -42,30 +42,31 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
-                         @if (Auth::guest())
+                         @if (Auth::guard('web_admin')->user())
                             <!-- <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li> -->
-                            <li>Nie si prihlásený</li>
-                        @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                    {{ Auth::guard('web_admin')->user()->name }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
-                                        <a href="{{ route('logout') }}"
+                                        <a href=""
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                             Logout
                                         </a>
 
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        <form id="logout-form" action="/admin_logout" method="POST" style="display: none;">
                                             {{ csrf_field() }}
                                         </form>
                                     </li>
                                 </ul>
                             </li>
+
+                        @else
+                            <li>Nie si prihlásený</li>
                         @endif
                     </ul>
                 </div>
