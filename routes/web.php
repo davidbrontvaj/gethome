9f72e682071ed94d00724d84ef79c95104be1b44
oliverd <dendis.oliver@gmail.com>
2017-07-17 21:09:19 +0200

    17072017_RealtyModel_ODE
    Pridaný nový model, nový controller a nový migračný file pre správu realít v adminovi. V súčasnosti funguje v controlleri konštruktor a pridávanie novej nehnuteľnosti

--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,30 @@ Route::post('admin', 'AdminAuth\LoginController@login');
 Route::get('admin/administrators/create', 'AdminAuth\RegisterController@showRegistrationForm');
 Route::post('admin/administrators/create', 'AdminAuth\RegisterController@register');
 
+//Admin RealtyRegistration - new Realty
+Route::get('admin/AddRealty','RealtiesController\AdminRealtiesController@showAddForm');
+Route::post('admin/AddRealty','RealtiesController\AdminRealtiesController@addNewRealty');
+
+// Admin & users Middlewares
+// /*******/
+
+
+
+//Logged in admins cannot access or send requests these pages
+Route::group(['middleware' => 'admin_guest'], function() {
+
+	Route::get('admin', 'AdminAuth\LoginController@showLoginForm');
+	Route::post('admin', 'AdminAuth\LoginController@login');
+
+});
+
+
+//Only logged in admins can access or send requests to these pages
+Route::group(['middleware' => 'admin_auth'], function(){
+
+	Route::post('admin_logout', 'AdminAuth\LoginController@logout');
+	Route::get('/admin/dashboard', function(){
+	  return view('admin.dashboard');
+	});
+
+});
